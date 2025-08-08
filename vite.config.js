2420377a01046669a4ba8b6ccbce9ff9dc2c87a9
mariamsanoko <mariam.sanoko@gmail.com>
2025-08-08 18:57:02 +0200

    u

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
-  base: '/coach-ia-ghpages/',
-  },
-);
+  base: '/coach-ia-ghpages/', // ← TRÈS IMPORTANT
+})

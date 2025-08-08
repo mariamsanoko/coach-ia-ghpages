74008fe8239822e5fd2ec8f7fbcbd89a231d7fb0
mariamsanoko <mariam.sanoko@gmail.com>
2025-08-08 17:19:59 +0200

    Update vite.config.js

--- react-coachia-cicd/vite.config.js
+++ react-coachia-cicd/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/coach-ia-ghpages/",
+  base:"/coach-ia-ghpages/react-coachia-cicd/",
   server : {
     open:true,
     port: 3001,

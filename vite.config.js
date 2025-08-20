39b9b742e0d3ce8ffa6decb995d8f08509b16bb0
mariamsanoko <mariam.sanoko@gmail.com>
2025-08-20 15:36:45 +0200

    delete

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// vite.config.js
-export default defineConfig({
-  plugins: [react()],
-  base: '/coach-ia-ghpages/', 
-})
